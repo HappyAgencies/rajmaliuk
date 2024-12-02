014f5763483c59f445aea471766059422ea0bec8
Roman Salom <romansalom@Romans-MacBook-Pro.local>
2024-12-02 17:42:04 -0300

    dynamic text works ,

--- components/dinamicText/dinamicText.jsx
+++ components/dinamicText/dinamicText.jsx
@@ -1,17 +1,53 @@
 'use client'
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
 import styles from './dinamicText.module.css';
 import { Mont } from '../../app/fonts/fonts';
 
 const DinamicText = () => {
+  const fullText = "I help companies solve complex technical & GTM challenges";
+  const words = fullText.split(" "); // Dividir el texto en palabras
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const docHeight = document.documentElement.scrollHeight;
+    
+    const scrollPercent = (scrollPosition / (docHeight - windowHeight)) * 100;
+    setScrollPercentage(scrollPercent);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Cálculo de cuántas palabras deben volverse visibles
+  const wordsToDisplay = Math.floor((scrollPercentage / 100) * words.length);
+
   return (
     <div className={styles.dinamic}>
       <div className={styles.dinamicContenedor}>
-      <h2 className={`${styles.phrase} ${Mont.className}`} >I help companies solve complex tachnical & GTM challenges</h2>
+        <h2 className={`${styles.phrase} ${Mont.className}`}>
+          {words.map((word, index) => (
+            <span
+              key={index}
+              style={{
+                color: index < wordsToDisplay ? 'white' : '#313131', // Inicialmente en gris oscuro
+                transition: 'color 0.3s ease-out'
+              }}
+            >
+              {word}{" "}
+            </span>
+          ))}
+        </h2>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
 export default DinamicText;
