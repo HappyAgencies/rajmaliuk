3aa5f089f67552ebf39d9547909ae584d40ef47b
Roman Salom <romansalom@Romans-MacBook-Pro.local>
2024-12-03 15:45:47 -0300

    white text scroll fixed

--- components/dinamicText/dinamicText.jsx
+++ components/dinamicText/dinamicText.jsx
@@ -1,21 +1,26 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './dinamicText.module.css';
 import { Mont } from '../../app/fonts/fonts';
 
 const DinamicText = () => {
   const fullText = "I help companies solve complex technical & GTM challenges";
-  const words = fullText.split(" "); // Dividir el texto en palabras
+  const words = fullText.split(" ");
   const [scrollPercentage, setScrollPercentage] = useState(0);
+  const textRef = useRef(null); 
 
   const handleScroll = () => {
-    const scrollPosition = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const docHeight = document.documentElement.scrollHeight;
-    
-    const scrollPercent = (scrollPosition / (docHeight - windowHeight)) * 100;
-    setScrollPercentage(scrollPercent);
+    if (textRef.current) {
+      const rect = textRef.current.getBoundingClientRect(); 
+      const sectionTop = rect.top;
+      const windowHeight = window.innerHeight;
+
+      if (sectionTop <= windowHeight && sectionTop + rect.height >= 0) {
+        const scrollPercent = ((windowHeight - sectionTop) / windowHeight) * 100;
+        setScrollPercentage(scrollPercent);
+      }
+    }
   };
 
   useEffect(() => {
@@ -26,18 +31,17 @@ const DinamicText = () => {
     };
   }, []);
 
-  // Cálculo de cuántas palabras deben volverse visibles
   const wordsToDisplay = Math.floor((scrollPercentage / 100) * words.length);
 
   return (
-    <div className={styles.dinamic}>
+    <div className={styles.dinamic} ref={textRef}>
       <div className={styles.dinamicContenedor}>
         <h2 className={`${styles.phrase} ${Mont.className}`}>
           {words.map((word, index) => (
             <span
               key={index}
               style={{
-                color: index < wordsToDisplay ? 'white' : '#313131', // Inicialmente en gris oscuro
+                color: index < wordsToDisplay ? 'white' : '#313131', // Cambiar el color en base al porcentaje
                 transition: 'color 0.3s ease-out'
               }}
             >
