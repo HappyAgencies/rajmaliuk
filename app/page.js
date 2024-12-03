99a9643bdb2ff851362f3051923eb045d6f97ece
nicolashappyagencies <nicolas@happyagencies.com>
2024-12-03 13:32:27 -0300

    add card section, not finished

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Header from "../components/header";
 import { Hero } from "../components/hero/hero";
 import DinamicText from "../components/dinamicText/dinamicText";
 import Experience from "../components/experience/experience";
+import Cards from "../components/cards/cards";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Hero/>
       <DinamicText></DinamicText>
       <Experience/>
+      <Cards/>
       </div>
     </>
   );
--- components/cards/cards.jsx
+++ components/cards/cards.jsx
@@ -1,43 +1,98 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styles from './cards.module.css';
+import { Mont } from '../../app/fonts/fonts';
 
 const Cards = () => {
-  return (
-    <>
-    <div>
-        <div></div>
-        <h2>Brands Led and Founded</h2>
-        <div></div>
-    </div>
-    <div>
-        <div>
-            <div>
-                <div>
-                    <p>MTC</p>
-                    <p>Outsourcing Platform</p>
-                </div>
-                <Image src="/assets/imgs/mtc.png" width={58} height={40} />
-            </div>
-            <div>
-                <p>MTC is an outsourcing platform that helps businesses delegate key tasks to experts. With skilled professionals, MTC optimizes processes, cuts costs, and boosts productivity, letting companies focus on core activities.</p>
-                <p>2019 - NOW</p>
-            </div>
-        </div>
-        <div>
-            <div>
-                <div>
-                    <p>SuperTek</p>
-                    <p>Crypto mining technical services</p>
-                </div>
-                <Image src="/assets/imgs/supertek.png" width={136} height={29}/>
-            </div>
-            <div>
-                <p>SuperTek provides expert technical services for cryptocurrency mining, ensuring optimal performance, maintenance, and troubleshooting to help mining businesses maximize profitability.</p>
-                <p>2017 - NOW</p>
-            </div>
-        </div>
-    </div>
-    </>
-  )
-}
+  // Estado para rastrear la tarjeta que está en hover
+  const [hoveredCard, setHoveredCard] = useState(null);
 
-export default Cards
+  const handleMouseEnter = (cardId) => {
+    setHoveredCard(cardId);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredCard(null);
+  };
+
+  return (
+    <div className="cardsSection">
+      <div className={styles.cardSectionTitle}>
+        <div className={styles.bar1}></div>
+        <h2 className={`${Mont.className} ${styles.brands}`}>Brands Led and Founded</h2>
+        <div className={styles.bar2}></div>
+      </div>
+      <div className={styles.cardsContainer}>
+        {/* Tarjeta 1 */}
+        <div
+          className={`${Mont.className} ${styles.card}`}
+          onMouseEnter={() => handleMouseEnter(1)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className={styles.cardTopRow}>
+            <div>
+              <p className={styles.cardBrand}>MTC</p>
+              <p className={styles.brandSubtitle}>Outsourcing Platform</p>
+            </div>
+            <Image src="/assets/imgs/mtc.png" alt="" width={58} height={40} />
+          </div>
+          <div className={styles.imageDiv}>
+            {hoveredCard === 1 && (
+              <Image 
+              src="/assets/imgs/man.png" 
+              alt="MTC Hover" 
+              style={{ maxHeight: '540px', width: 'auto', objectFit: 'cover' }} 
+              height={540} 
+              width={800} 
+            />
+            )}
+          </div>
+          <div className={styles.cardBottomRow}>
+            <p className={styles.cardText}>
+              MTC is an outsourcing platform that helps businesses delegate key tasks to experts. With skilled
+              professionals, MTC optimizes processes, cuts costs, and boosts productivity, letting companies focus on
+              core activities.
+            </p>
+            <p className={styles.cardYear}>2019 - NOW</p>
+          </div>
+        </div>
+
+        {/* Tarjeta 2 */}
+        <div
+          className={`${Mont.className} ${styles.card}`}
+          onMouseEnter={() => handleMouseEnter(2)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className={styles.cardTopRow}>
+            <div>
+              <p className={styles.cardBrand}>SuperTek</p>
+              <p className={styles.brandSubtitle}>Crypto mining technical services</p>
+            </div>
+            <Image src="/assets/imgs/supertek.png" alt="" width={250} height={30} />
+          </div>
+          <div className={styles.imageDiv}>
+            {hoveredCard === 2 && (
+               <Image 
+               src="/assets/imgs/man.png" 
+               alt="MTC Hover" 
+               style={{ maxHeight: '540px', width: 'auto', objectFit: 'cover' }} 
+               height={540} 
+               width={800} 
+             />
+            )}
+          </div>
+          <div className={styles.cardBottomRow}>
+            <p className={styles.cardText}>
+              SuperTek provides expert technical services for cryptocurrency mining, ensuring optimal performance,
+              maintenance, and troubleshooting to help mining businesses maximize profitability.
+            </p>
+            <p className={styles.cardYear}>2017 - NOW</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
