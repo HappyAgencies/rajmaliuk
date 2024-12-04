8c1e8c2704224924e76c65b7df510945a1c2b21a
nicolashappyagencies <nicolas@happyagencies.com>
2024-12-04 17:07:39 -0300

    add: small features to the mobile header

--- components/header.jsx
+++ components/header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
   return (
     <div id='header' className={styles['header-div']}>
       <a href="" className={`${styles.name} ${allura.className}`}>Ariel Rajmaliuk</a>
+      <p className={`${Mont.className} ${styles.position}`} >CEO @happyagencies & Fractional <span className={styles.positionSpan}>CTO/CMO</span></p>
       <a className={`${styles.consultation} ${Mont.className}`} href="">Schedule a consultation</a>
     </div>
   );
