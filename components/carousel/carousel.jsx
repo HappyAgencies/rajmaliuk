34036453a405e7eb9a6e190fc164349413fe0bc4
nicolashappyagencies <nicolas@happyagencies.com>
2024-12-05 17:54:14 -0300

    add: mobile view fixes

--- components/carousel/carousel.jsx
+++ components/carousel/carousel.jsx
@@ -6,7 +6,7 @@ const Carousel = () => {
   const slides = [
     '/assets/imgs/castcapital.png',
     '/assets/imgs/nexafin.png',
-    '/assets/imgs/supertek.png',
+    '/assets/imgs/supertekBox.png',
     // tus imágenes o contenido
   ];
 
