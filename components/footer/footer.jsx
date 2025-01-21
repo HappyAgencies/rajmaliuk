edbb95e724050b3718df148c08969aae7c84a12d
Roman Salom <romansalom@Romans-MacBook-Pro.local>
2025-01-21 12:00:43 -0300

    fix : footer , when hover border color change

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -11,7 +11,7 @@ const Footer = (props) => {
         <a className={`${Mont.className} ${styles.button}`} href="#header">{props.rightText}</a>
         <div className={styles.socials}>
             <a href=""><Image src="/assets/imgs/linkedin2.png" width={20} height={20} alt=""/></a>
-            <a href=""><Image src="/assets/imgs/twitter2.png" width={20} height={20} alt=""/></a>
+            <a href=""><Image src="/assets/imgs/twitter2.png" width={16} height={10} alt=""/></a>
         </div>
      
     </div>
