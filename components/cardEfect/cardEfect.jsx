fc603476761b85d3372f57e2294f8262d871d57a
Roman Salom <romansalom@Romans-MacBook-Pro.local>
2025-01-21 17:51:50 -0300

    cards needs more details

--- components/cardEfect/cardEfect.jsx
+++ components/cardEfect/cardEfect.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import styles from './cardEfect.module.css';
+"use client";
+import React, { useState } from "react";
+import styles from "./cardEfect.module.css";
 import { Mont } from "../../app/fonts/fonts";
 
 export default function CardEfect() {
-  return (
-    <>
-      <div className={`${Mont.className} ${styles.contenedor}`}>
-        <div className={`${Mont.className} ${styles.titlecontainer}`}>
-          <h3 className={styles.title}>
-            A decade of experience. United to Unlock a new era in brand growth
-          </h3>
-        </div>
+  const [expanded, setExpanded] = useState(false);
 
-        <div className={`${styles.card}`}>
-          <span className={` ${styles.cardNumber}`}>01</span>
-          <h4 className={` ${styles.cardTitle}`}>Areas of Expertise</h4>
-          <p className={styles.cardParagraph}>
-            An expert specialized in business strategy development, process
-            optimization, organizational change management, and digital
-            transformation.
-          </p>
-        </div>
-        
+  return (
+    <div className={`${Mont.className} ${styles.contenedor}`}>
+      {/* TÍTULO (Debe quedarse fijo) */}
+      <div className={`${Mont.className} ${styles.titlecontainer}`}>
+        <h3 className={styles.title}>
+          A decade of experience. United to Unlock a new era in brand growth
+        </h3>
       </div>
-    </>
+
+      {/* Contenedor de las tarjetas */}
+      <div
+        className={`${styles.cardsContainer} ${
+          expanded ? styles.expanded : ""
+        }`}
+        onClick={() => setExpanded(!expanded)}
+      >
+        {[1, 2, 3, 4].map((num) => (
+          <div key={num} className={`${styles.card} ${styles[`card${num}`]}`}>
+            <span className={styles.cardNumber}>0{num}</span>
+            <h4 className={styles.cardTitle}>Areas of Expertise</h4>
+            <p className={styles.cardParagraph}>
+              An expert specialized in business strategy development, process
+              optimization, organizational change management, and digital
+              transformation.
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
