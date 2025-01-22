3c05af5ab6c7e4ae12c83ce0a5a3bc616e36055a
Roman Salom <romansalom@Romans-MacBook-Pro.local>
2025-01-22 11:38:56 -0300

    card section with efect , desktop and mobile

--- components/cardEfect/cardEfect.jsx
+++ components/cardEfect/cardEfect.jsx
@@ -1,26 +1,65 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./cardEfect.module.css";
 import { Mont } from "../../app/fonts/fonts";
 
 export default function CardEfect() {
   const [expanded, setExpanded] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1200);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!sectionRef.current) return;
+
+      const section = sectionRef.current;
+      const rect = section.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      if (isMobile) {
+        // En pantallas menores a 1200px, activar cuando el 79% de la sección es visible
+        if (rect.top <= windowHeight * 0.21 && rect.bottom >= windowHeight * 1) {
+          setExpanded(true);
+        } else {
+          setExpanded(false);
+        }
+      } else {
+        // En pantallas más grandes, sigue el comportamiento original con el 50%
+        if (rect.top < windowHeight / 2 && rect.bottom > windowHeight / 2) {
+          setExpanded(true);
+        } else {
+          setExpanded(false);
+        }
+      }
+    };
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    handleResize(); // Detecta el tamaño inicial al cargar
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobile]);
 
   return (
-    <div className={`${Mont.className} ${styles.contenedor}`}>
-      {/* TÍTULO (Debe quedarse fijo) */}
+    <div ref={sectionRef} className={`${Mont.className} ${styles.contenedor}`}>
       <div className={`${Mont.className} ${styles.titlecontainer}`}>
         <h3 className={styles.title}>
           A decade of experience. United to Unlock a new era in brand growth
         </h3>
       </div>
 
-      {/* Contenedor de las tarjetas */}
       <div
         className={`${styles.cardsContainer} ${
-          expanded ? styles.expanded : ""
+          expanded ? (isMobile ? styles.expandedMobile : styles.expanded) : ""
         }`}
-        onClick={() => setExpanded(!expanded)}
       >
         {[4, 3, 2, 1].map((num) => (
           <div key={num} className={`${styles.card} ${styles[`card${num}`]}`}>
